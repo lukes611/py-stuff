52f96cf4aac1f3276d615922ea83e7685a831121
luke lincoln <luke@canva.com>
2019-09-19 13:00:19 +1000

    adding histogram example

---
+++ create_histogram_example.py
@@ -0,0 +1,16 @@
+import random
+
+def create_histogram(arr):
+  histogram = {}
+  for i in arr:
+    if i not in histogram:
+      histogram[i] = 0
+    histogram[i] += 1
+  return histogram
+
+
+
+numbers = [1,1,2,2,2,2,3,5,3,2,1,1,1,5,3]
+hg = create_histogram(numbers)
+print(numbers)
+print(hg)
